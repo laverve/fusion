7290f6f44bd5cac090201860a400c7c16eb67d03
Victor Pishuk <victor.pishuk@gmail.com>
2024-10-15 22:14:41 -0400

    fix(wpelling-spree): improves wordlist

--- games/spelling-spree/src/SpellingSpreeWordsList.tsx
+++ games/spelling-spree/src/SpellingSpreeWordsList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext } from "react";
 import classnames from "classnames";
 import { Button } from "@nextui-org/react";
 import { SpellingSpreeContext } from "./SpellingSpree.context";
@@ -10,14 +10,21 @@ export type SpellingSpreeWordsListProps = {
 export const SpellingSpreeWordsList: React.FC<SpellingSpreeWordsListProps> = ({
     onWordClick
 }: SpellingSpreeWordsListProps) => {
-    const { completedWords } = useContext(SpellingSpreeContext);
+    const { completedWords, words } = useContext(SpellingSpreeContext);
+
+    const isWordComplete = useCallback(
+        (word: string) => {
+            return completedWords.includes(word);
+        },
+        [completedWords, words]
+    );
 
     return (
         <div
             data-testid="testid-wordsearch-words-list"
             className={classnames("box-border", "justify-center", "items-center", "flex-1", "gap-1")}
         >
-            {completedWords.map((word) => (
+            {words.map((word) => (
                 <Button
                     key={word}
                     color="default"
@@ -27,7 +34,7 @@ export const SpellingSpreeWordsList: React.FC<SpellingSpreeWordsListProps> = ({
                     className="m-1 px-2 py-1 !min-h-auto !h-auto !min-w-0 !w-auto"
                     onClick={() => onWordClick?.({ word })}
                 >
-                    {word}
+                    <span className={classnames({ "line-through": isWordComplete(word) })}>{word}</span>
                 </Button>
             ))}
         </div>
